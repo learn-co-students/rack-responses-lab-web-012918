c43859d7efd1fc0eb151062a8fe01f71219dd6c7
github username <github email address>
2018-02-16 11:05:30 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,19 @@
+require "pry"
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+
+
+    # binding.pry
+    if Time.now.hour <= 12
+        resp.write "Good Morning!\n"
+    else
+        resp.write "Good Afternoon!\n"
+    end
+        resp.finish
+  end
+
+
+
+end
